964dcec10b7e4f9c6b909cd84c9596625b464f3d
Md Adil <mdadilsss2003@gmail.com>
2025-07-21 12:46:54 +0530

    Add Doctor Dashboard and Profile functionality with SVG icons
    - Implement DoctorContext for managing doctor-related state and API calls.
    - Create DoctorDashboard component to display earnings, appointments, and patients.
    - Add DoctorProfile component for viewing and editing doctor profile information.
    - Introduce new SVG icons for earnings, list, patients, and tick.
    - Remove unused react.svg file.
    - Implement login functionality for both Admin and Doctor roles.

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import cancel_icon from './cancel_icon.svg'
+import list_icon from './list_icon.svg'
+import tick_icon from './tick_icon.svg'
+import appointments_icon from './appointments_icon.svg'
+import earning_icon from './earning_icon.svg'
+import patients_icon from './patients_icon.svg'
+
+export const assets = {
+    cancel_icon,
+    patients_icon,
+    list_icon,
+    tick_icon,
+    appointments_icon,
+    earning_icon
+}
